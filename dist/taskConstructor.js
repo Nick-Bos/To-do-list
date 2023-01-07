7e2fbf0cf4f09401a4387dbfb4ae124fcc14dff4
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2023-01-07 16:37:23 +1030

    initial commit

---
+++ dist/taskConstructor.js
@@ -0,0 +1,9 @@
+class taskMaker {
+  constructor(title, description, date) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
+}
+
+export { taskMaker };
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { newTask } from "../dist/createTask";
+import { taskMaker } from "../dist/taskConstructor";
+newTask();
