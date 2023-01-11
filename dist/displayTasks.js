9f62211e493a3f5bddd14ccd14af56262b5237f7
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2023-01-11 12:38:19 +1030

    feat: :zap: Add DisplayTasks.js
    Module displays tasks that are created and takes inputs from createTask.js and fills appropriate input fields

---
+++ dist/displayTasks.js
@@ -0,0 +1,74 @@
+const displayTasks = (a) => {
+  //task display items
+  const myArray = a;
+  const tasks = document.querySelector(".tasks");
+  const tasksContainer = document.createElement("div");
+  tasksContainer.id = "tasksContainer";
+  const list = document.createElement("li");
+  list.id = "listItems";
+  const anc = document.createElement("a");
+
+  //taskfocus window
+  const taskFocus = document.createElement("div");
+  const tfTitle = document.createElement("h3");
+  const tfDescLabel = document.createElement("p");
+  const tfDescBox = document.createElement("textarea");
+  const tfDate = document.createElement("input");
+  tfDate.type = "date";
+  const tfDelete = document.createElement("button");
+  const tfClose = document.createElement("button");
+
+  //append to Dom
+  tasks.appendChild(tasksContainer);
+  tasksContainer.appendChild(list);
+  list.appendChild(anc);
+
+  displayArr(myArray);
+
+  function displayArr(arg) {
+    for (let i = 0; i < arg.length; i++) {
+      let newTitle = arg[i].title;
+      let newDate = arg[i].date;
+      anc.id = [i];
+      anc.innerHTML = newTitle;
+    }
+  }
+
+  anc.addEventListener("click", (e) => {
+    for (let i = 0; i < myArray.length; i++) {
+      if (anc.id == [i]) {
+        tasks.appendChild(taskFocus);
+        taskFocus.appendChild(tfTitle);
+        tfTitle.innerHTML = myArray[i].title;
+        taskFocus.appendChild(tfDescLabel);
+        taskFocus.appendChild(tfDescBox);
+        taskFocus.appendChild(tfDate);
+        taskFocus.appendChild(tfClose);
+        taskFocus.appendChild(tfDelete);
+
+        tfDescLabel.innerHTML = "Description";
+        tfDescBox.innerHTML = String(myArray[i].description);
+
+        tfClose.innerHTML = "Close";
+        tfDelete.innerHTML = "Delete";
+
+        //deletes the task
+        tfDelete.addEventListener("click", () => {
+          tasks.removeChild(taskFocus);
+          tasks.removeChild(tasksContainer);
+          myArray.splice(i, 1, 0);
+        });
+
+        //closes the taskFocus window
+        tfClose.addEventListener("click", () => {
+          tasks.removeChild(taskFocus);
+        });
+      }
+
+      console.log(myArray);
+    }
+  });
+  console.log(myArray);
+};
+
+export { displayTasks };
